d90ca24f4bea0cb3e99be8e1903b3842b9affe00
traciklopez <traciklopez@gmail.com>
2015-04-03 12:33:58 -0700

    initial

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,23 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Baby Tradsies"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+  end
+
+test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Baby Tradsies"
   end
 
 end
